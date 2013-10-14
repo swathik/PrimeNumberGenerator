5ba7de20d1e745f1c4a7cca05a74307bd8875018
Swathi <swathu.k@gmail.com>
2013-10-14 14:44:27 +0100

    Update prime_number_generator.rb
    Formatting

--- lib/prime_number_generator.rb
+++ lib/prime_number_generator.rb
@@ -1,29 +1,29 @@
 class PrimeNumberGenerator
 
-	def generate_primes(count)
-		current 		 = 2
-		generated 	 = 0
-		prime_basket = []
-		while(true)
-			break if generated == count
-			if is_prime?(current)
-				prime_basket << current
-				generated += 1
-			end
-			current += 1
-		end
-		prime_basket
-	end
+  def generate_primes(count)
+    current      = 2
+    generated    = 0
+    prime_basket = []
+    while(true)
+      break if generated == count
+      if is_prime?(current)
+        prime_basket << current
+        generated += 1
+      end
+      current += 1
+    end
+    prime_basket
+  end
 
-	private
-		def is_prime?(number)
-			return false if( number > 2 && number.even?)
-			check_until = Math.sqrt(number).floor
-			values = (2..check_until).select do |n|
-				(number % n).zero?
-			end
-			!values.any?
-		end
+  private
+    def is_prime?(number)
+      return false if( number > 2 && number.even?)
+      check_until = Math.sqrt(number).floor
+      values = (2..check_until).select do |n|
+        (number % n).zero?
+      end
+      !values.any?
+    end
 end
 
 # start_time = Time.now
